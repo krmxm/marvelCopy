f9b3c6079e18e7e6dc4619494c80a25c8e685396
Kramarenko Maxim <kramarenko-90@inbox.ru>
2024-03-15 23:40:39 +0300

    fix descr in MarvelService

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -24,7 +24,7 @@ class MarvelService {
     _transformCharacter = (char) => {
         return {
             name: char.name,
-            description: char.description,
+            description: char.description ? char.description.length > 210 ? char.description.slice(0, 210) + '...' : char.description : 'No description',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url
